101ae3fcaab17deff3620c13d56066c606589e10
kwonhee1 <lkh8033@gmail.com>
2025-08-18 16:39:53 +0900

    feat : create dtos

--- src/main/java/TheFirstCommit/demo/family/entity/FamilyEntity.java
+++ src/main/java/TheFirstCommit/demo/family/entity/FamilyEntity.java
@@ -2,14 +2,18 @@ package TheFirstCommit.demo.family.entity;
 
 import TheFirstCommit.demo.BasedEntity;
 import TheFirstCommit.demo.family.PaymentDay;
+import TheFirstCommit.demo.user.entity.UserEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -37,5 +41,8 @@ public class FamilyEntity extends BasedEntity {
 
     @OneToOne(mappedBy = "family", targetEntity = ElderEntity.class)
     private ElderEntity elder;
+
+    @OneToMany
+    private List<UserEntity> member;
 
 }
---
+++ src/main/java/TheFirstCommit/demo/payment/dto/response/ResponsePaymentSummeryDto.java
@@ -0,0 +1,30 @@
+package TheFirstCommit.demo.payment.dto.response;
+
+import TheFirstCommit.demo.family.PaymentDay;
+import TheFirstCommit.demo.user.dto.response.ResponseUserProfileDto;
+import TheFirstCommit.demo.user.entity.UserEntity;
+import java.time.LocalDate;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class ResponsePaymentSummeryDto {
+
+    private PaymentDay paymentDay;
+    private LocalDate nextPaymentDay;
+    private LocalDate sincePaymentDay;
+    private ResponseUserProfileDto leader;
+
+    public ResponsePaymentSummeryDto of(PaymentDay paymentDay, UserEntity leader) {
+        ResponsePaymentSummeryDto dto = new ResponsePaymentSummeryDto();
+        dto.leader = ResponseUserProfileDto.of(leader);
+        dto.paymentDay = paymentDay;
+        dto.nextPaymentDay = paymentDay.getDay();
+        dto.sincePaymentDay = leader.getCreatedAt().toLocalDate();
+        return dto;
+    }
+
+}
